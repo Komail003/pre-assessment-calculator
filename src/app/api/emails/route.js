347bda03b093bf27ce5e175aa269c7d1dfc5436d
Komail003 <komailabbas376@gmail.com>
2024-08-27 17:03:20 +0500

    emailchanged

--- src/app/api/emails/route.js
+++ src/app/api/emails/route.js
@@ -44,8 +44,8 @@ console.log("PersonalData: ",PersonalData)
     // Send email to BCC (yourself or another recipient) with a different template
     const bccMailOptions = {
       from: "fahadtech.fts@gmail.com",
-      // to: "admin@denarowealth.com.au", // You can add multiple BCC recipients here if needed
-      to: "wahabhameed2160@gmail.com", // You can add multiple BCC recipients here if needed
+      to: "admin@denarowealth.com.au", // You can add multiple BCC recipients here if needed
+      // to: "wahabhameed2160@gmail.com", // You can add multiple BCC recipients here if needed
       subject,
       html: generateBccHTMLTemplate(PersonalData), // Use your BCC-specific template here
       attachments: [
